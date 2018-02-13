87c0f6b6b2a44a66cbed5a776137ebe2cf9d3d73
Igor Sidorenko <neither89@gmail.com>
2018-02-13 09:58:17 +0300

    Delete sendmail.sh

--- vulners-scanner/sendmail.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Igor Sidorenko
-python /root/vulners-scanner/vulners_over_ssh_scanner.py | mail -s "Отчет об уязвимостях ИС $(date +%d.%m.%Y)" -r "Vulners Scanner <no-reply@example.com>" admins@example.com
